c43f0692623c3ebe77ba6d42d79670ca542be7ca
cidy0106 <361193190@qq.com>
2020-05-19 16:07:42 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,8 @@
 cd /gost
 wget -O gost.tar.gz  http://github.com/ginuerzh/gost/releases/download/v${VER}/gost_${VER}_linux_amd64.tar.gz
 tar zxf gost.tar.gz 
+ls
+
 cd /gost/gost_${VER}_linux_amd64
 chmod +x gost
 #./gost -L $MODE://:$PORT?secrets=secrets.txt
