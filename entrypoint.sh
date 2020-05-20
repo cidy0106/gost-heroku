4be1d72ecd00d0212b8a7c755c73352943a4134d
cidy0106 <361193190@qq.com>
2020-05-20 15:36:52 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,10 @@ wget -O gost.gz https://github.com/ginuerzh/gost/releases/download/v2.11.0/gost-
 gunzip gost.gz
 #cd /gost/gost_${VER}_linux_amd64
 chmod +x gost
-#./gost -L $MODE://:$PORT?secrets=/gost/secrets.txt
-./gost -L $MODE://:$PORT
+
+cur_dir=$(cd `dirname $0`; pwd)
+echo ${cur_dir}
+cat ${cur_dir}/secrets.txt
+
+./gost -L $MODE://:$PORT?secrets=/gost/secrets.txt
+#./gost -L $MODE://:$PORT
