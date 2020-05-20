68d155a8edc37586028cc46ad7e1b4f51c86d72d
cidy0106 <361193190@qq.com>
2020-05-20 10:30:04 +0800

    fix some error

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,8 @@
 cd /gost
-wget -O gost.tar.gz https://github.com/ginuerzh/gost/releases/download/v2.11.0/gost-linux-amd64-${VER}.gz
-tar zxf gost.tar.gz 
+wget -O gost.gz https://github.com/ginuerzh/gost/releases/download/v2.11.0/gost-linux-amd64-${VER}.gz
+#tar zxf gost.tar.gz 
+gunzip gost.gz
 #cd /gost/gost_${VER}_linux_amd64
 chmod +x gost
-./gost -L $MODE://:$PORT?secrets=secrets.txt
-#./gost -L $MODE://:$PORT
+#./gost -L $MODE://:$PORT?secrets=/gost/secrets.txt
+./gost -L $MODE://:$PORT
